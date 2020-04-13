751cb9264f03f2c39b79988d3f7f9c63ec08dd43
SHIYI MAO <44790654+mao2021@users.noreply.github.com>
2020-04-13 19:07:49 -0400

    Add files via upload

---
+++ cs12bspr20Simulation-master/src/lesson19/AnalyzeSimulation.java
@@ -0,0 +1,12 @@
+package lesson19;
+
+public class AnalyzeSimulation{
+
+
+// run 100 simulations
+//average number of days it takes until there are no new infections     numOfDays
+//average number of people infected and eventually recovered            numOfInfected
+//maximum number of people infected           peakInfectedLevel
+
+
+}
